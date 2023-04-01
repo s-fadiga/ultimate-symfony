bb8f2770c2c69964dd7098a946d2605ab0c4d5b4
votre nom <votre email>
2023-04-01 02:10:13 +0200

    premiere page

---
+++ src/Controller/TestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Taxes\Calculator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController {
+
+    protected $calculator;
+
+ 
+
+    /**
+     * @Route("/home/{age<\d+>?0}", name="test",
+     * methods={"GET", "POST"})
+     */
+    public function home(Request $request, $age, Calculator $calculator) {
+
+        $tva = $calculator->calcul(200);
+
+        dd($tva);
+
+        return new Response("vous avez $age ans ");
+    }
+
+    /**
+     * @Route("/hello/{prenom?world}", name="hello")
+     */
+    public function hello(Request $request, $prenom) {
+        return new Response("hello $prenom");
+    }
+}
